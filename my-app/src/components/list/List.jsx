105bf022b2f54b54f4aa57cf5c93384897f9b474
tejas01g <gargtejasvi076@gmail.com>
2024-06-01 10:35:33 +0530

    first

---
+++ my-app/src/components/list/List.jsx
@@ -0,0 +1,14 @@
+import "./list.css";
+import Userinfo from "./userinfo/Userinfo";
+import ChatList  from "./chatList/ChatList";
+
+const List =() => {
+    return (
+        <div className="list">
+            <Userinfo/>
+            <ChatList/>
+        </div>
+    )
+}
+
+export default List
