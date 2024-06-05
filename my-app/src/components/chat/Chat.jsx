e18386a3fcbde3d12e7ca8543ae72e222af93a8a
tejas01g <gargtejasvi076@gmail.com>
2024-06-05 21:36:51 +0530

    design

--- my-app/src/components/chat/Chat.jsx
+++ my-app/src/components/chat/Chat.jsx
@@ -1,6 +1,18 @@
+import { useState } from "react";
 import "./chat.css";
+import EmojiPicker from "emoji-picker-react";
 
 const Chat =() => {
+
+    const[open,setopen] = useState(false);
+    const[text,setText] = useState("");
+
+    const handleEmoji = (e) =>{
+        setText((prev) => prev + e.emoji);
+        setopen(false)
+    };
+
+    console.log(text)
     return (
         <div className="chat">
             <div className="top">
@@ -14,19 +26,60 @@ const Chat =() => {
                 <div className="icons">
                     <img src="./phone.png" alt =""/>
                     <img src="./video.png" alt =""/>
-                    <img src="./info.png" alt =""/>
+                    <img srlc="./info.png" alt =""/>
                 </div>
             </div>
-            <div className="center"></div>
+            <div className="center">
+                <div className="message">
+                    <img src="./avatar.png" alt="" />
+                    <div className="text">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel animi iusto porro, deleniti amet eveniet eos quo repudiandae repellat vero eum, ex aut aperiam laboriosam nostrum? Quis officia voluptas ex!</p>
+                        <span>10:45 AM</span>
+                    </div>
+                </div>
+
+                <div className="message own">
+                    <div className="text">
+                    {/* <img src="https://play-lh.googleusercontent.com/C9CAt9tZr8SSi4zKCxhQc9v4I6AOTqRmnLchsu1wVDQL0gsQ3fmbCVgQmOVM1zPru8UH" alt="" /> */}
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores veniam dolor aliquid commodi aperiam harum corrupti, dolorum dicta sequi voluptatum fugiat iure obcaecati nostrum rem beatae. Sunt incidunt consequatur blanditiis.</p>
+                        <span>10:00 AM</span>
+                    </div>
+                </div>
+
+                <div className="message">
+                    <img src="./avatar.png" alt="" />
+                    <div className="text">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel animi iusto porro, deleniti amet eveniet eos quo repudiandae repellat vero eum, ex aut aperiam laboriosam nostrum? Quis officia voluptas ex!</p>
+                        <span>10:45 AM</span>
+                    </div>
+                </div>
+
+                <div className="message own">
+                <div className="text">
+                <img src="https://play-lh.googleusercontent.com/C9CAt9tZr8SSi4zKCxhQc9v4I6AOTqRmnLchsu1wVDQL0gsQ3fmbCVgQmOVM1zPru8UH" alt="" />
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae accusamus ab quia fugit, velit perferendis unde quasi aspernatur asperiores, architecto corrupti. Quo eaque cupiditate sed nostrum voluptates aliquid voluptate quasi.</p>
+                        <span>10:00 AM</span>
+                    </div>
+                </div>
+
+            </div>
             <div className="bottom">
                 <div className="icons">
                    <img src="./img.png" alt=""/>
                    <img src="./camera.png" alt=""/>
                    <img src="./mic.png" alt=""/>
                 </div>
-                <input type="text" placeholder="Type a message..."/>
+                <input type="text" placeholder="Type a message..." onChange={e=>setText(e.target.value)}
+                value={text}/>
+                
                 <div className="emoji">
-                    <img src="./emoji.png" alt=""/>
+                    <img src="./emoji.png"
+                     alt=""
+                     onClick={() =>setopen(prev=>!prev)}
+                     />
+                     <div className="picker">
+                    <EmojiPicker open={open} onEmojiClick={handleEmoji}/>
+                     </div>
                 </div>
                 <button className="sendButton">Send</button>
             </div>
--- my-app/src/components/detail/Detail.jsx
+++ my-app/src/components/detail/Detail.jsx
@@ -2,8 +2,69 @@ import "./detail.css";
 
 const Detail =() => {
     return (
-        <div className="detail">Detail</div>
+        <div className="detail">Detail
+        <div className="user">
+            <img src="./avatar.png" alt="" />
+            <h2>Jane Doe</h2>
+            <p>Lorem ipsum dolor sit amet consectet</p>
+        </div>
+        <div className="info">
+        <div className="option">
+            <div className="title">
+                <span>Chat Setttings</span>
+                <img src="./arrowUp.png" alt="" />
+            </div>
+            </div>   
+
+            <div className="option">
+            <div className="title">
+                <span>Privacy & help</span>
+                <img src="./arrowUp.png" alt="" />
+            </div>
+            </div>   
+
+             <div className="option">
+                <div className="title">
+                <span>Shared photos</span>
+                <img src="./arrowUp.png" alt="" />
+                <div/>
+                </div>
+            <div className="photos">
+                <div className="photoItem">
+                    <div className="photoDetail">
+                    <img 
+                    src="https://dp-pic.com/wp-content/uploads/2024/01/a-boy-picked-a-gun-in-his-hands-in-attitude-dp-by-dp-pic-boys-attitude-dp-3.jpg" 
+                    alt="" 
+                    />
+                    <span>photo_2024_2.png</span>
+                    </div>
+                <img src="./download.png" alt="" className="icons" />
+                </div>
+                <div className="photoItem">
+                    <div className="photoDetail">
+                    <img 
+                    src="https://dp-pic.com/wp-content/uploads/2024/01/a-boy-picked-a-gun-in-his-hands-in-attitude-dp-by-dp-pic-boys-attitude-dp-3.jpg" 
+                    alt="" 
+                    />
+                    <span>photo_2024_2.png</span>
+                    </div>
+                <img src="./download.png" alt="" className="icons" />
+                </div>
+            </div>
+            </div>
+            
+             <div className="option">
+            <div className="title">
+                <span>Shared Files</span>
+                <img src="./arrowUp.png" alt="" />
+            </div>
+            </div>
+            <div/> 
+
+            <button>Block User</button>  
+        </div>
+        </div>
     )
 }
 
-export default Detail
+export default Detail;
